a2d224bb73bb1a3dad43c185f4d77697e2fa70d3
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-01 14:36:23 +0800

    chore: Add components and feature content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 const colors = require('./src/infrastructure/themes/colors');
 
 module.exports = {
-  content: ['./App.tsx'],
+  content: [
+    './App.tsx',
+    './src/components/*.jsx',
+    './src/features/**/**/*.jsx',
+  ],
   theme: {
     extend: {
       colors,
