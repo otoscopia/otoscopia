7f9d3706f6a47aa9186b5b40645325e2f8ac1097
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-01 14:35:52 +0800

    feat: Add screenOptions to hide header

--- src/infrastructure/navigation/Accounts.jsx
+++ src/infrastructure/navigation/Accounts.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
+
 function TemplateComponent() {
   return null;
 }
 
-export default function Accounts() {
-  const Stack = createNativeStackNavigator();
+const screenOptions = {
+  headerShown: false,
+};
 
+export default function Accounts() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={screenOptions}>
       <Stack.Screen name="template" component={TemplateComponent} />
     </Stack.Navigator>
   );
