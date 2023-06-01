3db0322eb139c0d2b1aee1ff524037d03f6075e0
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-01 13:33:15 +0800

    feat: Add PaperProvider from react-native-paper

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
 import AuthenticationProvider from './src/services/authentication/Authentication-Context';
 import Navigation from './src/infrastructure/navigation/Navigation';
 
 function App() {
   return (
     <AuthenticationProvider>
-      <Navigation />
+      <PaperProvider>
+        <Navigation />
+      </PaperProvider>
     </AuthenticationProvider>
   );
 }
