1ee75e02bc624cbcc077760bcc2ffdda3443c66a
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-01 18:28:08 +0800

    feat: Add Sign up screen

--- src/infrastructure/navigation/Accounts.jsx
+++ src/infrastructure/navigation/Accounts.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignIn from '../../features/Accounts/screens/Sign-In';
+import SignUp from '../../features/Accounts/screens/Sign-Up';
 
 const Stack = createNativeStackNavigator();
 
 function SignInComponent() {
   return <SignIn />;
+}
+
+function SignUpComponent() {
+  return <SignUp />;
 }
 
 const screenOptions = {
@@ -15,7 +20,8 @@ const screenOptions = {
 export default function Accounts() {
   return (
     <Stack.Navigator screenOptions={screenOptions}>
-      <Stack.Screen name="template" component={SignInComponent} />
+      <Stack.Screen name="Sign In" component={SignInComponent} />
+      <Stack.Screen name="Sign Up" component={SignUpComponent} />
     </Stack.Navigator>
   );
 }
