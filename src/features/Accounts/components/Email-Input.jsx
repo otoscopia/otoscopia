f156766f0823f86a0c4c1377dd77a4196b90b651
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-01 15:12:29 +0800

    feat: Add email input component built on top of user text input

---
+++ src/features/Accounts/components/Email-Input.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TextInput from '../../../components/Text-Input';
+
+export default function EmailInput({ onChangeText, onEndEditing, showHelper }) {
+  return (
+    <TextInput
+      label="Email Address"
+      autoCapitalize="none"
+      autoComplete="email"
+      inputMode="email"
+      keyboardType="email-address"
+      onChangeText={onChangeText}
+      onEndEditing={onEndEditing}
+      placeholder="@deped.edu.ph"
+      textContentType="emailAddress"
+      right={<TextInput.Icon icon="email" forceTextInputFocus={false} />}
+      showHelper={showHelper}
+      helperMessage="Invalid email address"
+    />
+  );
+}
+
+EmailInput.propTypes = {
+  onChangeText: PropTypes.func.isRequired,
+  onEndEditing: PropTypes.func.isRequired,
+  showHelper: PropTypes.bool.isRequired,
+};
