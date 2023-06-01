76ac29144ff087b9fc46a141d7b7b74c0b923374
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-01 12:52:03 +0800

    feat: Add authentication context for authenticating users

---
+++ src/services/authentication/Authentication-Context.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import React, {createContext, useMemo, useState} from 'react';
+
+export const AuthenticationContext = createContext();
+
+export default function AuthenticationProvider({children}) {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState(null);
+
+  const contextValue = useMemo(() => ({isAuthenticated: !!user}), [user]);
+
+  return (
+    <AuthenticationContext.Provider value={contextValue}>
+      {children}
+    </AuthenticationContext.Provider>
+  );
+}
+
+AuthenticationProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
