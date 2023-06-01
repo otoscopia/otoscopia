a32d40ae885f9ba9e08bd591af9f3f0d8199e786
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-01 11:39:13 +0800

    chore: Initialize eslint using npm init @eslint/config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,36 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true,
+    'react-native/react-native': true,
+  },
+  extends: [
+    '@react-native-community',
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:react-native/all',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-native'],
+  rules: {
+    'react/jsx-filename-extension': [
+      1,
+      {extensions: ['.ts', '.tsx', '.js', '.jsx']},
+    ],
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+    'react-native/no-single-element-style-arrays': 2,
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+  },
 };
