8bde68bcadbb95abf5e5d35ed6ef8f68bef0dc70
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-01 18:00:04 +0800

    feat: Add Sign In screen

---
+++ src/features/Accounts/screens/Sign-In.jsx
@@ -0,0 +1,107 @@
+import React, { useContext, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { View } from 'react-native';
+import { Divider } from 'react-native-paper';
+import AnimatedLottieView from 'lottie-react-native';
+import ScreenContainer from '../../../components/Screen-Container';
+import { AuthenticationContext } from '../../../services/authentication/Authentication-Context';
+import Header from '../components/Header';
+import EmailInput from '../components/Email-Input';
+import PasswordInput from '../components/Password-Input';
+import Button from '../../../components/Button';
+import PressableText from '../../../components/Pressable-Text';
+import AnimatedIllustration from '../../../assets/sign-in-illustration.json';
+import colors from '../../../infrastructure/themes/colors';
+
+export default function SignIn() {
+  const navigation = useNavigation();
+  const { signInRequest } = useContext(AuthenticationContext);
+
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState(false);
+
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordSecure, setPasswordSecure] = useState(true);
+
+  const signInFunction = () => {
+    if (emailError === false && passwordError === false) {
+      signInRequest(email.toLowerCase().trim(), password.toLowerCase().trim());
+    }
+  };
+
+  return (
+    <ScreenContainer>
+      <View className="flex-1 justify-center p-4 space-y-4">
+        <View>
+          <AnimatedLottieView
+            source={AnimatedIllustration}
+            autoPlay
+            loop
+            className="w-full"
+          />
+        </View>
+
+        <View>
+          <Divider />
+        </View>
+
+        <View>
+          <Header
+            title="Hello"
+            secondTitle="Login Now"
+            subtitle="Please fill in the form to create a new account and continue"
+          />
+        </View>
+
+        <View>
+          <EmailInput
+            onChangeText={setEmail}
+            onEndEditing={() =>
+              !email.includes('@deped.edu.ph')
+                ? setEmailError(true)
+                : setEmailError(false)
+            }
+            showHelper={emailError}
+          />
+        </View>
+
+        <View>
+          <PasswordInput
+            onChangeText={setPassword}
+            onPress={() => setPasswordSecure(!passwordSecure)}
+            onEndEditing={() =>
+              !(password.length >= 8)
+                ? setPasswordError(true)
+                : setPasswordError(false)
+            }
+            secureTextEntry={passwordSecure}
+            showHelper={passwordError}
+          />
+        </View>
+
+        <View>
+          <Button text="Sign In" onPress={signInFunction} />
+        </View>
+
+        <View>
+          <PressableText
+            text="Don&lsquo;t have have an account?"
+            strongText=" Register here"
+            onPress={() => navigation.navigate('Sign Up')}
+            center
+          />
+        </View>
+
+        <View>
+          <PressableText
+            text="Forgot password?"
+            strongText=" Click here"
+            onPress={() => navigation.navigate('Forgot Password')}
+            center
+          />
+        </View>
+      </View>
+    </ScreenContainer>
+  );
+}
