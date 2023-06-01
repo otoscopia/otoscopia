e7098d5d828894f1457aad324fddbc6e3d2c009d
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-01 19:04:26 +0800

    feat: Add Loading animation checkpoint

--- src/infrastructure/navigation/Accounts.jsx
+++ src/infrastructure/navigation/Accounts.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignIn from '../../features/Accounts/screens/Sign-In';
 import SignUp from '../../features/Accounts/screens/Sign-Up';
+import LoadingScreen from '../../components/Loading-Animation';
+import { AuthenticationContext } from '../../services/authentication/Authentication-Context';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,15 +15,26 @@ function SignUpComponent() {
   return <SignUp />;
 }
 
+function LoadingComponent() {
+  return <LoadingScreen />;
+}
+
 const screenOptions = {
   headerShown: false,
 };
 
 export default function Accounts() {
+  const { isLoading } = useContext(AuthenticationContext);
   return (
     <Stack.Navigator screenOptions={screenOptions}>
-      <Stack.Screen name="Sign In" component={SignInComponent} />
-      <Stack.Screen name="Sign Up" component={SignUpComponent} />
+      {isLoading ? (
+        <Stack.Screen name="Loading Animation" component={LoadingComponent} />
+      ) : (
+        <>
+          <Stack.Screen name="Sign In" component={SignInComponent} />
+          <Stack.Screen name="Sign Up" component={SignUpComponent} />
+        </>
+      )}
     </Stack.Navigator>
   );
 }
