8b7cdbcd0223e53b580c734470c76a0175ffc5a4
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-01 14:13:06 +0800

    feat: Add Accounts screens native stack navigation

--- src/infrastructure/navigation/Navigation.jsx
+++ src/infrastructure/navigation/Navigation.jsx
@@ -2,12 +2,13 @@ import React, { useContext } from 'react';
 import { Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthenticationContext } from '../../services/authentication/Authentication-Context';
+import Accounts from './Accounts';
 
 export default function Navigation() {
   const { isAuthenticated } = useContext(AuthenticationContext);
   return (
     <NavigationContainer>
-      {isAuthenticated ? <Text>True</Text> : <Text>False</Text>}
+      {isAuthenticated ? <Text>True</Text> : <Accounts />}
     </NavigationContainer>
   );
 }
