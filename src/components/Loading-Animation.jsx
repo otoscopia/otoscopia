6569bc12c84fb588f971ddef7724c198b2e4ce2e
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-03 19:03:57 +0800

    refractor: Remove ScreenContainer and made the component a Modal component

--- src/components/Loading-Animation.jsx
+++ src/components/Loading-Animation.jsx
@@ -1,32 +1,43 @@
 import React from 'react';
 import AnimatedLottieView from 'lottie-react-native';
 import { View, useColorScheme } from 'react-native';
+import { Modal, Portal } from 'react-native-paper';
+import PropTypes from 'prop-types';
 import LoadingAnimation from '../assets/loading-animation.json';
 import colors from '../infrastructure/themes/colors';
-import ScreenContainer from './Screen-Container';
 
-export default function LoadingScreen() {
+export default function LoadingScreen({ visible }) {
   const colorScheme = useColorScheme();
 
   return (
-    <ScreenContainer enableKeyboardAvoidingView={false} scrollEnabled>
-      <View className="flex-1 items-center justify-center">
-        <AnimatedLottieView
-          source={LoadingAnimation}
-          autoPlay
-          loop
-          className="w-full"
-          colorFilters={[
-            {
-              keypath: 'Shape Layer 2',
-              color:
-                colorScheme === 'dark'
-                  ? colors.dark.background
-                  : colors.light.background,
-            },
-          ]}
-        />
-      </View>
-    </ScreenContainer>
+    <Portal>
+      <Modal visible={visible}>
+        <View className="h-full bg-light-surface dark:bg-dark-surface justify-center items-center">
+          <AnimatedLottieView
+            source={LoadingAnimation}
+            autoPlay
+            loop
+            className="w-full"
+            colorFilters={[
+              {
+                keypath: 'Shape Layer 2',
+                color:
+                  colorScheme === 'dark'
+                    ? colors.dark.surface
+                    : colors.light.background,
+              },
+            ]}
+          />
+        </View>
+      </Modal>
+    </Portal>
   );
 }
+
+LoadingScreen.propTypes = {
+  visible: PropTypes.bool,
+};
+
+LoadingScreen.defaultProps = {
+  visible: false,
+};
