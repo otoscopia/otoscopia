74467e2993d6319ba9fd4b4c59b9001500cd7ab7
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-03 19:02:50 +0800

    feat: Remove loading stack screen

--- src/infrastructure/navigation/Accounts.jsx
+++ src/infrastructure/navigation/Accounts.jsx
@@ -1,9 +1,7 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignIn from '../../features/Accounts/screens/Sign-In';
 import SignUp from '../../features/Accounts/screens/Sign-Up';
-import LoadingScreen from '../../components/Loading-Animation';
-import { AuthenticationContext } from '../../services/authentication/Authentication-Context';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,26 +13,15 @@ function SignUpComponent() {
   return <SignUp />;
 }
 
-function LoadingComponent() {
-  return <LoadingScreen />;
-}
-
 const screenOptions = {
   headerShown: false,
 };
 
 export default function Accounts() {
-  const { isLoading } = useContext(AuthenticationContext);
   return (
     <Stack.Navigator screenOptions={screenOptions}>
-      {isLoading ? (
-        <Stack.Screen name="Loading Animation" component={LoadingComponent} />
-      ) : (
-        <>
-          <Stack.Screen name="Sign In" component={SignInComponent} />
-          <Stack.Screen name="Sign Up" component={SignUpComponent} />
-        </>
-      )}
+      <Stack.Screen name="Sign In" component={SignInComponent} />
+      <Stack.Screen name="Sign Up" component={SignUpComponent} />
     </Stack.Navigator>
   );
 }
