bc8590e1ba8bf8286df6501f6745c4eff9748d46
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-04 14:17:41 +0800

    feat: StatusBar now changes background according to the theme

--- App.tsx
+++ App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { StatusBar, useColorScheme } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import AuthenticationProvider from './src/services/authentication/Authentication-Context';
 import Navigation from './src/infrastructure/navigation/Navigation';
+import colors from './src/infrastructure/themes/colors';
 
 function App() {
+  const colorScheme = useColorScheme();
+
   return (
     <AuthenticationProvider>
       <PaperProvider>
         <Navigation />
       </PaperProvider>
+      <StatusBar
+        backgroundColor={
+          colorScheme === 'dark'
+            ? colors.dark.background
+            : colors.light.background
+        }
+      />
     </AuthenticationProvider>
   );
 }
