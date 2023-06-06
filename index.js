505bb77b58cbe0079b7d84fa406d90af3efe3fb2
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-06 16:53:01 +0800

    feat: Add Main function that returns the App inside the SafeAreaProvider

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 /**
  * @format
  */
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import App from './App.tsx';
+import { name as appName } from './app.json';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+function Main() {
+  return (
+    <SafeAreaProvider>
+      <App />
+    </SafeAreaProvider>
+  );
+}
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Main);
