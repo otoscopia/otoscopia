c4fa74334575898c61db3c737a9eb44943779107
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-06 18:12:04 +0800

    feat: Add un optimized SplashScreen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusBar, useColorScheme } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import AuthenticationProvider from './src/services/authentication/Authentication-Context';
 import Navigation from './src/infrastructure/navigation/Navigation';
 import colors from './src/infrastructure/themes/colors';
+import SplashScreen from './src/features/SplashScreen';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
   const colorScheme = useColorScheme() === 'dark';
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  if (isLoading) {
+    return <SplashScreen />;
+  }
 
   return (
     <AuthenticationProvider>
