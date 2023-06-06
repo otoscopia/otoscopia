a57d896c5b1691f79c02e7e670bae79bf3b7906c
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-06 20:34:22 +0800

    feat: Add initialWindowMetrics to speed up the initial renders

--- index.js
+++ index.js
@@ -3,13 +3,16 @@
  */
 import React from 'react';
 import { AppRegistry } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {
+  SafeAreaProvider,
+  initialWindowMetrics,
+} from 'react-native-safe-area-context';
 import App from './App.tsx';
 import { name as appName } from './app.json';
 
 function Main() {
   return (
-    <SafeAreaProvider>
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
       <App />
     </SafeAreaProvider>
   );
