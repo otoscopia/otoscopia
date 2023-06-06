5e17a747d88a1dcb6bb1887e8a6fb52ed5cd61c0
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-06 22:36:29 +0800

    feat: Add Password screen

---
+++ src/features/Accounts/screens/Password.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { Text } from 'react-native-paper';
+import AnimatedLottieView from 'lottie-react-native';
+import ScreenContainer from '../../../components/Screen-Container';
+import Header from '../components/Header';
+import PasswordInput from '../components/Password-Input';
+import Illustration from '../../../assets/password-animation.json';
+import Button from '../../../components/Button';
+
+export default function Password() {
+  const { params: email } = useRoute();
+
+  const navigation = useNavigation();
+
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordSecure, setPasswordSecure] = useState(true);
+
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [confirmPasswordError, setConfirmPasswordError] = useState(false);
+  const [confirmPasswordSecure, setConfirmPasswordSecure] = useState(true);
+
+  return (
+    <ScreenContainer>
+      <View className="flex-1 p-4 justify-center space-y-4">
+        <View>
+          <AnimatedLottieView
+            source={Illustration}
+            autoPlay
+            loop
+            className="w-full"
+          />
+        </View>
+
+        <View>
+          <Header
+            title="Set your password"
+            subtitle="Please enter your password"
+          />
+        </View>
+
+        <View>
+          <PasswordInput
+            onChangeText={setPassword}
+            onPress={() => setPasswordSecure(!passwordSecure)}
+            onEndEditing={() =>
+              !(password.length >= 8)
+                ? setPasswordError(true)
+                : setPasswordError(false)
+            }
+            secureTextEntry={passwordSecure}
+            showHelper={passwordError}
+          />
+        </View>
+
+        <View>
+          <PasswordInput
+            onChangeText={setConfirmPassword}
+            onPress={() => setConfirmPasswordSecure(!confirmPasswordSecure)}
+            onEndEditing={() =>
+              !(confirmPassword.length >= 8)
+                ? setConfirmPasswordError(true)
+                : setConfirmPasswordError(false)
+            }
+            secureTextEntry={confirmPasswordSecure}
+            showHelper={confirmPasswordError}
+          />
+        </View>
+
+        <View>
+          <Text>At least 8 characters long</Text>
+          <Text>Both uppercase and lowercase letter</Text>
+          <Text>At least one number or symbol</Text>
+        </View>
+
+        <View>
+          <Button
+            text="Submit"
+            onPress={() => navigation.navigate('User Information')}
+          />
+        </View>
+      </View>
+    </ScreenContainer>
+  );
+}
