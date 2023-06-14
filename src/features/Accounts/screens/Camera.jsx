023bad908ba1406f91c5b1400a30e9217e69c3b8
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-06-15 02:58:58 +0800

    feat: Add Camera screen for sign up

---
+++ src/features/Accounts/screens/Camera.jsx
@@ -0,0 +1,41 @@
+import { TouchableOpacity, View } from 'react-native';
+import React, { useRef } from 'react';
+import { RNCamera } from 'react-native-camera';
+import { useNavigation } from '@react-navigation/native';
+import ScreenContainer from '../../../components/Screen-Container';
+
+export default function Camera() {
+  const cameraRef = useRef(RNCamera);
+  const navigation = useNavigation();
+
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const options = { quality: 0.5, base64: true };
+      const imageData = await cameraRef.current.takePictureAsync(options);
+
+      navigation.navigate('Profile Picture', { capturedImage: imageData.uri });
+    }
+  };
+
+  return (
+    <ScreenContainer>
+      <View className="flex-1 py-8 flex-col bg-dark-background">
+        <RNCamera
+          ratio="4:3"
+          className="flex-1"
+          type={RNCamera.Constants.Type.front}
+          captureAudio={false}
+          ref={cameraRef}
+        />
+
+        <View className="flex-0 flex-row justify-center">
+          <TouchableOpacity
+            className="flex-0 bg-white rounded-full p-4 py-4 align-center m-4"
+            onPress={takePicture}>
+            <View className="bg-black w-8 h-8 rounded-full" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScreenContainer>
+  );
+}
