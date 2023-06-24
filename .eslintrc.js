502d2de8ba4a25fa878d77fad35224aafc095efe
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-25 02:05:29 +0800

    chore: update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,29 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  env: {
+    es2021: true,
+  },
+  extends: [
+    '@react-native',
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': [
+      1,
+      {extensions: ['.ts', '.tsx', '.js', '.jsx']},
+    ],
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+  },
 };
