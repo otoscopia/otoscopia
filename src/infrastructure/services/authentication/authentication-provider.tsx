ba5581d5bf741354085ef0b1c342dbdd12954fe4
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-26 00:21:06 +0800

    feat: Add AuthenticationProvider for authenticating user

---
+++ src/infrastructure/services/authentication/authentication-provider.tsx
@@ -0,0 +1,33 @@
+import React, {createContext, useMemo, useState} from 'react';
+
+interface AuthenticationContextValues {
+  isAuthenticated: boolean;
+  isLoading: boolean;
+}
+
+export const AuthenticationContext =
+  createContext<AuthenticationContextValues | null>(null);
+
+export default function AuthenticationProvider({
+  children,
+}: React.PropsWithChildren<{
+  children: React.ReactNode;
+}>): JSX.Element {
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const contextValue = useMemo(
+    () => ({
+      // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+      isAuthenticated: !!user,
+      isLoading,
+    }),
+    [user, isLoading],
+  );
+
+  return (
+    <AuthenticationContext.Provider value={contextValue}>
+      {children}
+    </AuthenticationContext.Provider>
+  );
+}
