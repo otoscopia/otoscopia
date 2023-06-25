b83bea2ac47eed2a806306861b29cfdab8bd05d5
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-26 01:34:02 +0800

    feat: Add colors

---
+++ src/infrastructure/themes/colors.js
@@ -0,0 +1,24 @@
+module.exports = {
+  primary: '#008080',
+  'primary-variant': '#005858',
+  secondary: '#FF5733',
+  'secondary-variant': '#DB3D15',
+
+  light: {
+    background: '#FFFBFE',
+    surface: '#F5F5F5',
+    onPrimary: '#FFFFFF',
+    onSecondary: '#1C1B1F',
+    onBackground: '#1C1B1F',
+    onSurface: '#1C1B1F',
+  },
+
+  dark: {
+    background: '#1C1B1F',
+    surface: '#2B2A30',
+    onPrimary: '#E6E1E5',
+    onSecondary: '#E6E1E5',
+    onBackground: '#E6E1E5',
+    onSurface: '#E6E1E5',
+  },
+};
