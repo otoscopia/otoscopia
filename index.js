fdc4270378679221e8befed328f50885ccd7e80d
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-26 01:27:14 +0800

    feat: Initialize react-native-safe-area-context

--- index.js
+++ index.js
@@ -2,8 +2,21 @@
  * @format
  */
 
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {
+  SafeAreaProvider,
+  initialWindowMetrics,
+} from 'react-native-safe-area-context';
 
-AppRegistry.registerComponent(appName, () => App);
+function Main() {
+  return (
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <App />
+    </SafeAreaProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
