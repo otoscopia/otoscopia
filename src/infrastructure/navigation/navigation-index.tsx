39da33228399017e1a1e946a83d4b69c9cc0dbb3
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-06-26 19:09:54 +0800

    feat: Add Accounts stack navigator

--- src/infrastructure/navigation/navigation-index.tsx
+++ src/infrastructure/navigation/navigation-index.tsx
@@ -2,13 +2,14 @@ import {Text} from 'react-native';
 import React, {useContext} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {AuthenticationContext} from '../services/authentication/authentication-provider';
+import Accounts from './accounts';
 
 export default function Navigation(): React.JSX.Element {
   const {isAuthenticated} = useContext(AuthenticationContext);
 
   return (
     <NavigationContainer>
-      {isAuthenticated ? <Text>True</Text> : <Text>False</Text>}
+      {isAuthenticated ? <Text>True</Text> : <Accounts />}
     </NavigationContainer>
   );
 }
