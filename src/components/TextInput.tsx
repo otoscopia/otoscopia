a4e5f17cf7468daf73af461b9fadfa640a9a28ff
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-06-26 21:42:55 +0800

    feat: Add user TextInput

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import {HelperText, TextInput as PaperInput} from 'react-native-paper';
+
+interface TextInputProps {
+  label: string;
+  autoCapitalize: string;
+  autoComplete: string;
+  autoCorrect: boolean;
+  editable: boolean;
+  importantForAutofill: string;
+  inputMode: string;
+  keyboardType: string;
+  maxLength: number;
+  onChangeText: () => any;
+  onEndEditing: () => any;
+  placeholder: string;
+  secureTextEntry: boolean;
+  textContentType: string;
+  left: React.JSX.Element;
+  right: React.JSX.Element;
+  showHelper: boolean;
+  helperMessage: string;
+}
+
+const defaultProps: TextInputProps = {
+  autoCapitalize: 'words',
+  autoComplete: 'off',
+  autoCorrect: false,
+  editable: true,
+  importantForAutofill: 'yes',
+  inputMode: 'text',
+  keyboardType: 'default',
+  maxLength: 32,
+  secureTextEntry: false,
+  left: () => null,
+  right: () => null,
+  showHelper: false,
+  label: '',
+  onChangeText: () => null,
+  onEndEditing: () => null,
+  placeholder: '',
+  textContentType: '',
+  helperMessage: '',
+};
+
+TextInput.defaultProps = defaultProps;
+
+export default function TextInput({
+  label,
+  autoCapitalize,
+  autoComplete,
+  autoCorrect,
+  editable,
+  importantForAutofill,
+  inputMode,
+  keyboardType,
+  maxLength,
+  onChangeText,
+  onEndEditing,
+  placeholder,
+  secureTextEntry,
+  textContentType,
+  left,
+  right,
+  showHelper,
+  helperMessage,
+}: TextInputProps): React.JSX.Element {
+  return (
+    <>
+      <PaperInput
+        mode="outlined"
+        label={label}
+        autoCapitalize={autoCapitalize}
+        autoComplete={autoComplete}
+        autoCorrect={autoCorrect}
+        caretHidden
+        editable={editable}
+        enablesReturnKeyAutomatically
+        importantForAutofill={importantForAutofill}
+        inputMode={inputMode}
+        keyboardType={keyboardType}
+        maxLength={maxLength}
+        onChangeText={onChangeText}
+        onEndEditing={onEndEditing}
+        placeholder={placeholder}
+        secureTextEntry={secureTextEntry}
+        textContentType={textContentType}
+        left={left}
+        right={right}
+        error={showHelper}
+      />
+      {showHelper && <HelperText>{helperMessage}</HelperText>}
+    </>
+  );
+}
+
