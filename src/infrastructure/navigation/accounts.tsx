7d356d5ede8e5a1e8605593d27cd2f1907357ebe
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-06-30 11:58:06 +0800

    feat: Add SignUpComponent

--- src/infrastructure/navigation/accounts.tsx
+++ src/infrastructure/navigation/accounts.tsx
@@ -1,11 +1,16 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import SignIn from '../../features/Accounts/screens/Sign-In';
+import SignUp from '../../features/Accounts/screens/Sign-Up';
 
 const Stack = createNativeStackNavigator();
 
 function SignInComponent(): React.JSX.Element {
   return <SignIn />;
+}
+
+function SignUpComponent(): React.JSX.Element {
+  return <SignUp />;
 }
 
 export default function Accounts(): React.JSX.Element {
@@ -16,6 +21,7 @@ export default function Accounts(): React.JSX.Element {
         animation: 'fade',
       }}>
       <Stack.Screen name="Sign In" component={SignInComponent} />
+      <Stack.Screen name="Sign Up" component={SignUpComponent} />
     </Stack.Navigator>
   );
 }
