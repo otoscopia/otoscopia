ccbd16970d71c9e29448738b171c69e6d6ffae64
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-06-30 20:29:10 +0800

    feat: Add VerificationComponent

--- src/infrastructure/navigation/accounts.tsx
+++ src/infrastructure/navigation/accounts.tsx
@@ -2,6 +2,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import SignIn from '../../features/Accounts/screens/Sign-In';
 import SignUp from '../../features/Accounts/screens/Sign-Up';
+import Verification from '../../features/Accounts/screens/Verification';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,10 @@ function SignUpComponent(): React.JSX.Element {
   return <SignUp />;
 }
 
+function VerificationComponent(): React.JSX.Element {
+  return <Verification />;
+}
+
 export default function Accounts(): React.JSX.Element {
   return (
     <Stack.Navigator
@@ -22,6 +27,7 @@ export default function Accounts(): React.JSX.Element {
       }}>
       <Stack.Screen name="Sign In" component={SignInComponent} />
       <Stack.Screen name="Sign Up" component={SignUpComponent} />
+      <Stack.Screen name="Verification" component={VerificationComponent} />
     </Stack.Navigator>
   );
 }
