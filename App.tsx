bc7cc41099b1267773a567b272f21d3231a54f78
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-07-09 17:46:19 +0800

    feat: Update add StatusBar cp;pr and barStyle

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { StatusBar, useColorScheme } from 'react-native';
 import AuthenticationProvider from './src/services/authentication-context';
 import NavigationContainer from './src/infrastructure/navigation/navigation-container';
+import colors from './src/infrastructure/navigation/themes/colors';
 
 export default function App(): React.JSX.Element {
+  const colorScheme = useColorScheme() === 'dark';
+
   return (
     <AuthenticationProvider>
       <NavigationContainer />
+      <StatusBar
+        backgroundColor={
+          colorScheme ? colors.dark.background : colors.light.background
+        }
+        barStyle={colorScheme ? 'light-content' : 'dark-content'}
+      />
     </AuthenticationProvider>
   );
 }
