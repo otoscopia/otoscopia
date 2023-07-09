4b4ec79781e9be2ab4aa2d9836e3dfa25d8b53c7
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-07-10 03:26:27 +0800

    feat: Add Provider context from react-native-paper

--- src/infrastructure/navigation/navigation-container.tsx
+++ src/infrastructure/navigation/navigation-container.tsx
@@ -1,13 +1,16 @@
 import React, { useContext } from 'react';
 import { NavigationContainer as NativeContainer } from '@react-navigation/native';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { AuthenticationContext } from '../../services/authentication-context';
 import AccountsNavigation from './accounts-navigation';
 
 export default function NavigationContainer(): React.JSX.Element {
   const { isAuthenticated } = useContext(AuthenticationContext);
   return (
-    <NativeContainer>
-      {isAuthenticated ? null : <AccountsNavigation />}
-    </NativeContainer>
+    <PaperProvider>
+      <NativeContainer>
+        {isAuthenticated ? null : <AccountsNavigation />}
+      </NativeContainer>
+    </PaperProvider>
   );
 }
