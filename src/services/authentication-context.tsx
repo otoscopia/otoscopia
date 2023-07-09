2479a70350ba0a342d0ced6cec5de31a3605da82
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-07-09 16:36:51 +0800

    feat: Create context AuthenticationProvider

---
+++ src/services/authentication-context.tsx
@@ -0,0 +1,31 @@
+import React, { createContext, useMemo, useState } from 'react';
+
+interface AuthenticationContextValues {
+  isAuthenticated: boolean;
+}
+
+const AuthenticationContextDefaultProps: AuthenticationContextValues = {
+  isAuthenticated: false,
+};
+
+const AuthenticationContext = createContext(AuthenticationContextDefaultProps);
+
+interface AuthenticationProviderProps {
+  children: React.ReactNode;
+}
+
+function AuthenticationProvider({
+  children,
+}: AuthenticationProviderProps): React.JSX.Element {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  const contextValue = useMemo(() => ({ isAuthenticated: !!user }), [user]);
+  return (
+    <AuthenticationContext.Provider value={contextValue}>
+      {children}
+    </AuthenticationContext.Provider>
+  );
+}
+
+export default AuthenticationProvider;
