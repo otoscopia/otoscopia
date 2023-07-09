603e658bd76f339a12e0d1207b5d56c640d4b8a1
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-07-09 17:45:54 +0800

    feat: Add colors.js

---
+++ src/infrastructure/navigation/themes/colors.js
@@ -0,0 +1,22 @@
+module.exports = {
+  primary: '#008080',
+  secondary: '#005858',
+
+  light: {
+    background: '#FFFBFE',
+    surface: '#F5F5F5',
+    onPrimary: '#FFFFFF',
+    onSecondary: '#1C1B1F',
+    onBackground: '#1C1B1F',
+    onSurface: '#1C1B1F',
+  },
+
+  dark: {
+    background: '#1C1B1F',
+    surface: '#2B2A30',
+    onPrimary: '#E6E1E5',
+    onSecondary: '#E6E1E5',
+    onBackground: '#E6E1E5',
+    onSurface: '#E6E1E5',
+  },
+};
