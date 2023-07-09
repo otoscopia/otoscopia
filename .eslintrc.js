a356bf87d583d4b970ea4d007604e7c23736c48e
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-07-09 15:07:41 +0800

    chore (update): Initialize .eslintrc.js
    using npm init @eslint/config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,31 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    '@react-native',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', 'tsx'] }],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        bracketSpacing: true,
+      },
+    ],
+  },
 };
