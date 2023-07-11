1d06536f5c7448d0a9135e120aff126e573b9624
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-07-11 20:15:53 +0800

    chore (update): Remove StatusBar component

--- App.tsx
+++ App.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import { StatusBar, useColorScheme } from 'react-native';
 import AuthenticationProvider from './src/services/authentication-context';
 import NavigationContainer from './src/infrastructure/navigation/navigation-container';
-import colors from './src/infrastructure/themes/colors';
 
 export default function App(): React.JSX.Element {
-  const colorScheme = useColorScheme() === 'dark';
-
   return (
     <AuthenticationProvider>
       <NavigationContainer />
-      <StatusBar
-        backgroundColor={
-          colorScheme ? colors.dark.background : colors.light.background
-        }
-        barStyle={colorScheme ? 'light-content' : 'dark-content'}
-      />
     </AuthenticationProvider>
   );
 }
