f6118daa9a4414a9c59ae84d1ee664fc8eae02a2
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-07-11 20:13:41 +0800

    feat: Update add loggedInRequest

--- src/services/authentication-context.tsx
+++ src/services/authentication-context.tsx
@@ -1,14 +1,19 @@
 import React, { createContext, useEffect, useMemo, useState } from 'react';
 import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import { signInRequest } from './authentication-service';
 
 interface AuthenticationContextValues {
   isAuthenticated: boolean;
   isLoading: boolean;
+  loggedInRequest: (email: string, password: string) => void;
+  loggedInRequestError: string;
 }
 
 const AuthenticationContextDefaultProps: AuthenticationContextValues = {
   isAuthenticated: false,
   isLoading: false,
+  loggedInRequest: () => {},
+  loggedInRequestError: '',
 };
 
 export const AuthenticationContext = createContext(
@@ -25,6 +30,7 @@ function AuthenticationProvider({
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState<FirebaseAuthTypes.User>();
   const [isLoading, setIsLoading] = useState(false);
+  const [loggedInRequestError, setLoggedInRequestError] = useState('');
 
   useEffect(() => {
     const subscriber = auth().onAuthStateChanged(userData => {
@@ -36,12 +42,25 @@ function AuthenticationProvider({
     return subscriber;
   }, [initializing]);
 
+  function loggedInRequest(email: string, password: string) {
+    setIsLoading(true);
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(error => {
+        setLoggedInRequestError(signInRequest(error));
+      });
+
+    setIsLoading(false);
+  }
+
   const contextValue = useMemo(
     () => ({
       isAuthenticated: !!user,
       isLoading,
+      loggedInRequest,
+      loggedInRequestError,
     }),
-    [user, isLoading],
+    [user, isLoading, loggedInRequestError],
   );
   return (
     <AuthenticationContext.Provider value={contextValue}>
