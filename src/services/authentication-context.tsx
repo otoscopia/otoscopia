fdb1b5ec6917fa4486832daeeb8f535819d93ea7
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-07-11 20:12:53 +0800

    feat: Update initialize @react-native-firebase

--- src/services/authentication-context.tsx
+++ src/services/authentication-context.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useMemo, useState } from 'react';
+import React, { createContext, useEffect, useMemo, useState } from 'react';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 
 interface AuthenticationContextValues {
   isAuthenticated: boolean;
@@ -22,11 +23,24 @@ function AuthenticationProvider({
   children,
 }: AuthenticationProviderProps): React.JSX.Element {
   const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState();
+  const [user, setUser] = useState<FirebaseAuthTypes.User>();
   const [isLoading, setIsLoading] = useState(false);
 
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(userData => {
+      if (userData !== null) {
+        setUser(userData);
+      }
+      setInitializing(false);
+    });
+    return subscriber;
+  }, [initializing]);
+
   const contextValue = useMemo(
-    () => ({ isAuthenticated: !!user, isLoading }),
+    () => ({
+      isAuthenticated: !!user,
+      isLoading,
+    }),
     [user, isLoading],
   );
   return (
